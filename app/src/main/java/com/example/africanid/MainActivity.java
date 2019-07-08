f55bcdba418bc842d1131d9a4fc077b6546d8355
Franz Vilela <franz.vilela@bolivia.assuresoft.com>
2019-07-08 17:59:33 -0400

    fixes

--- app/src/main/java/com/example/africanid/MainActivity.java
+++ app/src/main/java/com/example/africanid/MainActivity.java
@@ -16,25 +16,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.buttonS);
+        Button button = findViewById(R.id.button_submit);
 
-        final TextView label1 = findViewById(R.id.textLabel1);
-        final TextView label2 = findViewById(R.id.textLabel2);
-        final TextView txtBirthday = (TextView)findViewById(R.id.textBirthday);
-        final TextView txtGender = (TextView)findViewById(R.id.textGender);
+        final TextView label1 = findViewById(R.id.label_birthdayTittle);
+        final TextView label2 = findViewById(R.id.label_genderTittle);
+        final TextView txtBirthday = (TextView)findViewById(R.id.label_birthday);
+        final TextView txtGender = (TextView)findViewById(R.id.label_gender);
 
         label1.setVisibility(View.INVISIBLE);
         label2.setVisibility(View.INVISIBLE);
         txtBirthday.setVisibility(View.INVISIBLE);
         txtGender.setVisibility(View.INVISIBLE);
 
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText txtBx = (EditText)findViewById(R.id.txtBx);
-
-
+                EditText txtBx = (EditText)findViewById(R.id.txtBx_data);
                 String idNumber = txtBx.getText().toString();
 
                 if(idNumber.length()==13) {
@@ -46,20 +43,18 @@ public class MainActivity extends AppCompatActivity {
                     birthDay = birthDay.substring(4, 6) + "/" + birthDay.substring(2, 4) + "/" + birthDay.substring(0, 2);
                     txtBirthday.setText(birthDay);
 
-
                     String gender = idNumber.substring(6, 10);
                     if (gender.charAt(0) < '5') {
                         txtGender.setText("Female");
-                    } else
+                    } else {
                         txtGender.setText("Male");
+                    }
                 }
-                else
-                {
+                else{
                     Toast toast = Toast.makeText(getApplicationContext(),"ID invalid",Toast.LENGTH_SHORT);
                     toast.setGravity(0,2,2);
                     toast.show();
                 }
-
             }
         });
     }
